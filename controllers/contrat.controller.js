0c41e83df8b3bec031cafc3e70da8f76612891b0
Franck <franckvienot7@gmail.com>
2024-01-11 16:22:18 +0100

    added nombre de seeances to the contrat controller

--- controllers/contrat.controller.js
+++ controllers/contrat.controller.js
@@ -1,12 +1,20 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client")
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 const ContratController = {
   // Create a new contrat
   async create(req, res) {
-    const { id, name, description, published, begin_date, end_date, theme } =
-      req.body;
+    const {
+      id,
+      name,
+      description,
+      published,
+      begin_date,
+      end_date,
+      nombre_de_seances,
+      theme,
+    } = req.body
     try {
       const newContrat = await prisma.contrat.create({
         data: {
@@ -16,84 +24,91 @@ const ContratController = {
           published,
           begin_date,
           end_date,
+          nombre_de_seances,
           theme,
         },
-      });
-      res.status(201).json(newContrat);
+      })
+      res.status(201).json(newContrat)
     } catch (error) {
-      console.error(error);
+      console.error(error)
       res.status(500).json({
-        error: 'Une erreur est survenue lors de la création du contrat.',
-      });
+        error: "Une erreur est survenue lors de la création du contrat.",
+      })
     }
   },
 
   // Get all contrats
   async findAll(req, res) {
     try {
-      const page = parseInt(req.query.page) || 1;
-      const limit = parseInt(req.query.limit) || 20;
-      const offset = (page - 1) * limit;
+      const page = parseInt(req.query.page) || 1
+      const limit = parseInt(req.query.limit) || 20
+      const offset = (page - 1) * limit
 
       const contrats = await prisma.contrat.findMany({
         take: limit,
         skip: offset,
-      });
-      res.status(200).json(contrats);
+      })
+      res.status(200).json(contrats)
     } catch (error) {
-      console.error(error);
+      console.error(error)
       res.status(500).json({
-        error: 'Une erreur est survenue lors de la récupération des contrats.',
-      });
+        error: "Une erreur est survenue lors de la récupération des contrats.",
+      })
     }
   },
 
   // Get a contrat by ID
   async findOne(req, res) {
-    const { id } = req.params;
+    const { id } = req.params
     try {
       const contrat = await prisma.contrat.findUnique({
         where: { id: Number(id) },
-      });
+      })
       if (!contrat) {
-        return res.status(404).json({ error: 'Contrat non trouvé.' });
+        return res.status(404).json({ error: "Contrat non trouvé." })
       }
-      res.status(200).json(contrat);
+      res.status(200).json(contrat)
     } catch (error) {
-      console.error(error);
+      console.error(error)
       res.status(500).json({
-        error: 'Une erreur est survenue lors de la récupération du contrat.',
-      });
+        error: "Une erreur est survenue lors de la récupération du contrat.",
+      })
     }
   },
 
   // Get all published contrats
   async findAllPublished(req, res) {
     try {
-      const page = parseInt(req.query.page) || 1;
-      const limit = parseInt(req.query.limit) || 20;
-      const offset = (page - 1) * limit;
+      const page = parseInt(req.query.page) || 1
+      const limit = parseInt(req.query.limit) || 20
+      const offset = (page - 1) * limit
 
       const contrats = await prisma.contrat.findMany({
         where: { published: true },
         take: limit,
         skip: offset,
-      });
-      res.status(200).json(contrats);
+      })
+      res.status(200).json(contrats)
     } catch (error) {
-      console.error(error);
+      console.error(error)
       res.status(500).json({
-        error:
-          'Une erreur est survenue lors de la récupération des contrats publiés.',
-      });
+        error: "Une erreur est survenue lors de la récupération des contrats publiés.",
+      })
     }
   },
 
   // Update a contrat by ID
   async update(req, res) {
-    const { id } = req.params;
-    const { name, description, published, begin_date, end_date, theme } =
-      req.body;
+    const { id } = req.params
+    const {
+      name,
+      description,
+      published,
+      begin_date,
+      end_date,
+      nombre_de_seances,
+      theme,
+    } = req.body
     try {
       const updatedContrat = await prisma.contrat.update({
         where: { id: Number(id) },
@@ -103,47 +118,47 @@ const ContratController = {
           published,
           begin_date,
           end_date,
+          nombre_de_seances,
           theme,
         },
-      });
-      res.status(200).json(updatedContrat);
+      })
+      res.status(200).json(updatedContrat)
     } catch (error) {
-      console.error(error);
+      console.error(error)
       res.status(500).json({
-        error: 'Une erreur est survenue lors de la mise à jour du contrat.',
-      });
+        error: "Une erreur est survenue lors de la mise à jour du contrat.",
+      })
     }
   },
 
   // Delete a contrat by ID
   async delete(req, res) {
-    const { id } = req.params;
+    const { id } = req.params
     try {
       await prisma.contrat.delete({
         where: { id: Number(id) },
-      });
-      res.sendStatus(204);
+      })
+      res.sendStatus(204)
     } catch (error) {
-      console.error(error);
+      console.error(error)
       res.status(500).json({
-        error: 'Une erreur est survenue lors de la suppression du contrat.',
-      });
+        error: "Une erreur est survenue lors de la suppression du contrat.",
+      })
     }
   },
 
   // Delete all contrats
   async deleteAll(req, res) {
     try {
-      await prisma.contrat.deleteMany();
-      res.sendStatus(204);
+      await prisma.contrat.deleteMany()
+      res.sendStatus(204)
     } catch (error) {
-      console.error(error);
+      console.error(error)
       res.status(500).json({
-        error:
-          'Une erreur est survenue lors de la suppression de tous les contrats.',
-      });
+        error: "Une erreur est survenue lors de la suppression de tous les contrats.",
+      })
     }
   },
-};
+}
 
-module.exports = ContratController;
+module.exports = ContratController
